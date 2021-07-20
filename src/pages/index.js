8084b2aa71e9c286387e936a90ed6bc7de37a0ed
jupemara <jumpei.arashi@arashike.com>
2021-07-20 20:39:04 +0900

    tutorial5 is finished. move /blog to /

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,39 @@
+import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 import * as React from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout';
 
-const IndexPage = () => {
+const Blog = ({ data }) => {
   return (
-    <Layout title="Welcome to my Gatsby site!">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage alt="me" src="../../static/images/from-cms/img_1186.jpg" />
+    <Layout title="All of Blog Posts">
+      <ul>
+        {data.allMdx.nodes.map((v) => {
+          return (
+            <article key={v.id}>
+              <h2>{v.frontmatter.title}</h2>
+              <p>{v.frontmatter.pusblished_data}</p>
+              <MDXRenderer>{v.body}</MDXRenderer>
+            </article>
+          );
+        })}
+      </ul>
     </Layout>
   );
 };
 
-export default IndexPage;
+export const query = graphql`
+  query {
+    allMdx(sort: { order: DESC, fields: frontmatter___published_date }) {
+      nodes {
+        frontmatter {
+          title
+          published_date(formatString: "YYYY年MM月DD日")
+        }
+        id
+        body
+      }
+    }
+  }
+`;
+
+export default Blog;
