f28a3d93c3bd9fc9a83cad25826dd13db4fd3aec
jupemara <jumpei.arashi@arashike.com>
2021-07-25 23:11:49 +0900

    remove unnecessary ul

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,16 @@ import Layout from '../components/layout';
 const Index = ({ data }) => {
   return (
     <Layout title="All of Blog Posts">
-      <ul>
-        {data.allMdx.nodes.map((v) => {
-          return (
-            <article key={v.id}>
-              <Link to={v.frontmatter.path_alias}>
-                <h2>{v.frontmatter.title}</h2>
-              </Link>
-              <p>最終更新日: {v.frontmatter.published_date}</p>
-            </article>
-          );
-        })}
-      </ul>
+      {data.allMdx.nodes.map((v) => {
+        return (
+          <article key={v.id}>
+            <Link to={v.frontmatter.path_alias}>
+              <h2>{v.frontmatter.title}</h2>
+            </Link>
+            <p>最終更新日: {v.frontmatter.published_date}</p>
+          </article>
+        );
+      })}
     </Layout>
   );
 };
